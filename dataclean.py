c0bff061ab94547c8c52a1cabe5a876bd88b2e88
Jacob Neeley <jacneeley@outlook.com>
2023-11-25 12:32:43 -0600

    added new question q13, re-ordered columns, changed my analysis.

--- dataclean.py
+++ dataclean.py
@@ -11,16 +11,27 @@ path = "C:/Users/Owner/Documents/Python-Scripts/mkt_research/Phase2_mkt_research
 
 df = pd.read_csv(path, sep=',')
 
-#drops
-final_cols = ['ResponseId','Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10','Q11','Q12']
 
 #create a new df using the columns in list 'final_cols'.
+final_cols = ['ResponseId','Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10','Q11','Q12','Q13_1']
 df = df[df.columns.intersection(final_cols)]
 
-df.drop([1],inplace=True)
+
+#re-order columns
+df.rename(columns={'Q13_1':'Q13'},inplace=True)
+cols = df.columns.tolist()
+cols = cols[:5] + [cols[13]] + cols[5:13]
+df['Q13'][0] = 'How much did you spend on our products in the last 3 months?'
+df = df[cols]
+
+
+#drops
+df.drop([0,1],inplace=True)
 df.dropna(inplace=True)
 
 #clean
+df['Q13'] = pd.to_numeric(df['Q13'])
+
 likert_to_numeric = {'Strongly Disagree':1,
           'Somewhat disagree':2,
           'Neither agree nor disagree':3,
@@ -34,8 +45,9 @@ df[cols] = df[cols].replace(likert_to_numeric)
 ## AI Trust = Sum of row integers
 ## AI Trust score of 40 = 100% trust in AI = perfect score.
 ## AI Trust score of 0 = 0% trust in AI.
-sum_rows = df.iloc[:,5:].sum(axis=1)
+sum_rows = df.iloc[:,6:].sum(axis=1)
 sum_rows[0] = 'Score'
 df['AI Trust Score'] = sum_rows
+df['AI Trust Score'] = pd.to_numeric(df['AI Trust Score'])
 
 export_csv = df.to_csv(r'Customer_Trust_In_AI.csv',index=None,header=True,sep=',')
